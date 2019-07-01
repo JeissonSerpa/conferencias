da8ab4ca22a5c3c48706975bf3d7bd9f15661029
Jeisson Serpa <jeisson-serpa@outlook.com>
2019-07-01 13:52:27 -0500

    Retomando

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<h2>La mejor web de Conferencias</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		quis nostrud exercitation ullamco. laboris nisi ut aliquip ex ea commodo
 		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
 	</section>
 
